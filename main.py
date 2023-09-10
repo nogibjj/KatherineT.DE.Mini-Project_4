212ab8dc77844b816a928719235559408d6310bb
katherinetian540 <rui.tian@duke.edu>
2023-09-10 17:03:34 -0400

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-"""Main function goes here"""
+"""
+Main function goes here
+"""
+
+#import pandas and pyplot
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#define summary statistics function
+def sum_stats(data):
+    return data.describe()
 
 
-def func(input1):
-    """define an add function"""
-    return input1**2
+#define visualization function
+def visualization(data):
+    plt.hist(data["calories"], bins=10, color="purple")
+    plt.xlabel("Calories")
+    plt.ylabel("Frequency")
+    plt.title("Cereal Calories Histogram")
+    plt.show()
+    return
--- test_main.py
+++ test_main.py
@@ -1,9 +1,25 @@
 """
 Test Cases
 """
-from main import func
+#import functions
+from main import sum_stats, visualization
+import pandas as pd
 
-def test_answer():
-    """test function"""
-    assert func(3) == 9
-    assert func(2) == 4
+#define test function
+def test_sum_stats(data):
+    summary = sum_stats(data)
+
+    assert data['calories'].mean() == summary.loc['mean', 'calories']
+    assert data['protein'].median() == summary.loc['50%', 'protein'], "Median test failed"
+    assert data['rating'].min() == summary.loc['min', 'rating'], "Standard deviation test failed"
+    print("All Test passed!")
+
+#read dataset
+cereal = pd.read_csv("cereal.csv",sep=";")
+
+#test
+if __name__ == "__main__":
+    print(sum_stats(cereal))
+    visualization(cereal)
+    test_sum_stats(cereal)
+    
