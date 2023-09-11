e36dede9eb30a2a03968a75db28eb21acc6dbb2c
katherinetian540 <143833511+katherinetian540@users.noreply.github.com>
2023-09-10 23:31:22 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -10,7 +10,7 @@ def test_sum_stats():
     #read dataset
     cereal = pd.read_csv("cereal.csv",sep=";")
     summary = sum_stats(cereal)
-    assert cereal['calories'].mean() == summary.loc['mean', 'calories']
+    assert cereal['calories'].mean() == summary.loc['mean', 'calories'], "Mean test failed"
     assert cereal['protein'].median() == summary.loc['50%', 'protein'], "Median test failed"
     assert cereal['rating'].min() == summary.loc['min', 'rating'], "Standard deviation test failed"
     print("All Test passed!")
