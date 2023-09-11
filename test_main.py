ce388a485c5ef496d6d73d5f9a57123d114d7359
katherinetian540 <143833511+katherinetian540@users.noreply.github.com>
2023-09-10 23:28:35 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,20 +6,17 @@ from main import sum_stats, visualization
 import pandas as pd
 
 #define test function
-def test_sum_stats(data):
-    summary = sum_stats(data)
+def test_sum_stats():
+    #read dataset
+    cereal = pd.read_csv("cereal.csv",sep=";")
+    summary = sum_stats(cereal)
 
-    assert data['calories'].mean() == summary.loc['mean', 'calories']
-    assert data['protein'].median() == summary.loc['50%', 'protein'], "Median test failed"
-    assert data['rating'].min() == summary.loc['min', 'rating'], "Standard deviation test failed"
+    assert cereal['calories'].mean() == summary.loc['mean', 'calories']
+    assert cereal['protein'].median() == summary.loc['50%', 'protein'], "Median test failed"
+    assert cereal['rating'].min() == summary.loc['min', 'rating'], "Standard deviation test failed"
     print("All Test passed!")
-
-#read dataset
-cereal = pd.read_csv("cereal.csv",sep=";")
 
 #test
 if __name__ == "__main__":
-    print(sum_stats(cereal))
-    visualization(cereal)
-    test_sum_stats(cereal)
+    test_sum_stats()
     
