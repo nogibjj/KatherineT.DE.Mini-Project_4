a39e0a00f85147023b864ced3be0cf1b2b7c502c
katherinetian540 <143833511+katherinetian540@users.noreply.github.com>
2023-09-22 00:37:26 +0000

    5th

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ import pandas as pd
 from ydata_profiling import ProfileReport
 
 
-def read(csv,sep):
-    data = pd.read_csv(csv,sep=sep)
+def read(csv, sep):
+    data = pd.read_csv(csv, sep=sep)
     return data
 
 
 # read the CSV file
-cereal = read("cereal.csv",";")
+cereal = read("cereal.csv", ";")
 
 # compute summary statistics
 summary = cereal.describe()
@@ -27,10 +27,9 @@ plt.title("Cereal Calories Histogram")
 plt.savefig("Cereal Calories.png")
 plt.show()
 
+
 # generate a analyse report
 def generate_report():
     report = cereal.loc[:, ["calories"]]
-    profile = ProfileReport(
-        report, title="Cereal Calories", explorative=True
-    )
+    profile = ProfileReport(report, title="Cereal Calories", explorative=True)
     profile.to_file("data_profiling_report.html")
--- test_main.py
+++ test_main.py
@@ -3,25 +3,19 @@ Test goes here
 
 """
 
-from main import read,generate_report
+from main import read, generate_report
 
-#read dataset
+# read dataset
+
 
 def test_csv():
-    cereal = read("cereal.csv",";")
-    assert (
-        cereal["calories"].max() == 160
-    ), "Mean test failed"
-    assert (
-        cereal["protein"].median() == 3
-    ), "Median test failed"
-    assert (
-        cereal["fat"].count() == 77
-    ), "Count test failed"
+    cereal = read("cereal.csv", ";")
+    assert cereal["calories"].max() == 160, "Mean test failed"
+    assert cereal["protein"].median() == 3, "Median test failed"
+    assert cereal["fat"].count() == 77, "Count test failed"
     print("All Test passed!")
- 
+
+
 if __name__ == "__main__":
     test_csv()
     generate_report()
-
-
